da55fec68371691e7e6f64ea2cd7ea3cbaad7069
Rogerio Biscaia <rogeriombiscaia@gmail.com>
2017-09-22 17:20:05 +0100

    Erro em Autoload hcodebr classes

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,8 +6,18 @@ namespace Composer\Autoload;
 
 class ComposerStaticInitb0d50c0f4e496c94cb4e7ba94c64de24
 {
-    public static $fallbackDirsPsr4 = array (
-        0 => __DIR__ . '/..' . '/hcodebr/php-classes/src',
+    public static $prefixLengthsPsr4 = array (
+        'H' => 
+        array (
+            'Hcode\\' => 6,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'Hcode\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/hcodebr/php-classes/src',
+        ),
     );
 
     public static $prefixesPsr0 = array (
@@ -41,7 +51,8 @@ class ComposerStaticInitb0d50c0f4e496c94cb4e7ba94c64de24
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->fallbackDirsPsr4 = ComposerStaticInitb0d50c0f4e496c94cb4e7ba94c64de24::$fallbackDirsPsr4;
+            $loader->prefixLengthsPsr4 = ComposerStaticInitb0d50c0f4e496c94cb4e7ba94c64de24::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInitb0d50c0f4e496c94cb4e7ba94c64de24::$prefixDirsPsr4;
             $loader->prefixesPsr0 = ComposerStaticInitb0d50c0f4e496c94cb4e7ba94c64de24::$prefixesPsr0;
             $loader->classMap = ComposerStaticInitb0d50c0f4e496c94cb4e7ba94c64de24::$classMap;
 
