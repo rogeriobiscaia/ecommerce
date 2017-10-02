9b0fd5f77c49ae0941b963135f0335c45574a7b6
Rogerio Biscaia <rogeriombiscaia@gmail.com>
2017-10-02 18:39:15 +0100

    Added Products sorted by Category

--- site.php
+++ site.php
@@ -2,6 +2,7 @@
 
 use \Hcode\Page;
 use \Hcode\Model\Product;
+use \Hcode\Model\Category;
 
 
 $app->get('/', function() {
@@ -21,6 +22,24 @@ $app->get('/', function() {
 		]); //carrega o body
 
 });
+
+
+
+$app->get("/categories/:idcategory", function($idcategory){
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$page = new Page(); 
+
+	$page->setTpl("category", [
+		'category'=>$category->getValues(),
+		'products'=>Product::checkList($category->getProducts())
+		]); 
+	
+});
+
 
 
 
