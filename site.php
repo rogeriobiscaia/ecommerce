d37e1caac6249248ba513060b3532edfb9a69095
Rogerio Biscaia <rogeriombiscaia@gmail.com>
2017-10-03 22:07:03 +0100

    Added Shopping Cart

--- site.php
+++ site.php
@@ -3,6 +3,7 @@
 use \Hcode\Page;
 use \Hcode\Model\Product;
 use \Hcode\Model\Category;
+use \Hcode\Model\Cart;
 
 
 $app->get('/', function() {
@@ -116,6 +117,16 @@ $app->get("/products/:desurl", function($desurl){
 		'categories'=>$product->getCategories()
 		]);
 
+});
+
+
+$app->get("/cart", function(){
+
+	$cart = Cart::getFromSession();
+
+	$page = new Page();
+
+	$page->setTpl("cart");
 });
 
 
